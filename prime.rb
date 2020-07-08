d3ffbfb80c669c675b350e58d12195cdb06009cf
Joseph cohen <jojocohen57@gmail.com>
2020-07-08 18:59:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,15 @@
 def prime?)(num)
-  if num = 1 
-    return false 
-  end 
-  if num 
-end   
+  if num < 2
+    return false
+  end
+  
+  (2...num).each do |n|
+   if num % n == 0
+     return false
+   end
+ end 
+  
+  return true
+end
+
+  
